f8ff508cc408849047be905b75bb23501b73e90b
gilles <gilles.grandguillaume@gmail.com>
2016-03-21 00:23:44 -0300

    - Last version on all sub module - work with one thread by card detected - day rules integradted ans tested

--- serviceaccess.cpp
+++ serviceaccess.cpp
@@ -9,12 +9,15 @@ void ServiceAccess::check(const QString id)
     const QMetaObject *meta = obj->metaObject();
     QString caller = QString::fromUtf8(meta->className());
 
+    acceso->setDate(QDateTime::currentDateTime());
+
     if(checkMachineRules() == false)
     {
-        qWarning() << "Rules not passed";
-        Buzzer::instance()->bad();
-        emit finished();
-        return;
+        qWarning() << "Machine's Rules not passed";
+
+        acceso->setRut(id);
+        acceso->setIdAuth(Acceso::OUT_OF_HOURS);
+        finalizeResponse();
 
     } else {
 
@@ -26,16 +29,16 @@ void ServiceAccess::check(const QString id)
 
                 qDebug() << "UserCard finded in local database ...";
 
-                int userID = record.value("id_huella").toInt();
+                int fingerprintUserID = record.value("id_huella").toInt();
                 int autorizado = record.value("autorizado").toInt();
                 QString name = record.value("nombre").toString();
 
                 acceso->setRut(id);
                 acceso->setName(name);
                 acceso->setIdAuth(autorizado);
-                acceso->setDate(QDateTime::currentDateTime());
 
-                emit verifFingerprint(userID);
+                if(fingerprintUserID > 0)
+                    emit verifFingerprint(fingerprintUserID);
 
             } else {
 
@@ -82,101 +85,6 @@ bool ServiceAccess::checkMachineRules()
 
 void ServiceAccess::on_offline()
 {
-    /*
-    // Check valid credencial
-    if(!credenci.isEmpty()) {
-
-        accepted = true;
-        credencial.setId(credenci.value("credencial_id").toInt());
-
-        int sentido = credencial.sentido();
-        int periodo = credenci.value("periodo_id").toInt();
-        bool antiPassBack = credenci.value("antipassback").toBool();
-
-        QDate currentFecha = QDate::currentDate();
-        QTime currentHora = QTime::currentTime();
-
-
-        QString activacion = credenci.value("activacion").toString();
-        QString expiracion = credenci.value("expiracion").toString();
-        activacion = activacion.isEmpty()? currentFecha.toString("yyyy-MM-dd") : activacion;
-        expiracion = expiracion.isEmpty()? currentFecha.toString("yyyy-MM-dd") : expiracion;
-        QDate activacionDate = QDate::fromString(activacion, "yyyy-MM-dd");
-        QDate expiracionDate = QDate::fromString(expiracion, "yyyy-MM-dd");
-
-        qDebug() << "Id cred.     : " << credencial.id();
-        qDebug() << "activacion      : " << activacionDate;
-        qDebug() << "expiracion      : " << expiracionDate;
-        qDebug() << "Antipassback : " << antiPassBack;
-
-        // Check activacion/expiracion
-        accepted = (currentFecha >= activacionDate && currentFecha <= expiracionDate);
-        qDebug() << "--- Result activacion/expiracion : " << accepted;
-
-        // Check calendario TODO
-
-
-        // Check valid periodo
-        if(periodo != 0 && accepted)
-        {
-            qDebug() << "Current date : " << currentFecha;
-            qDebug() << "Current time : " << currentHora;
-
-            QSqlQuery horarios = Bdd::getHorarios(periodo,dia);
-
-            if(horarios.first())
-            {
-                accepted = false;
-                int cma = 0;
-
-                while (horarios.next()) {
-
-                    QString hi = horarios.value("horario_inicio").toString();
-                    QString hf = horarios.value("horario_fin").toString();
-                    QTime rhi = QTime::fromString(hi, "hh:mm");
-                    QTime rhf = QTime::fromString(hf, "hh:mm");
-
-                    qDebug() << "Valido desde " << hi << " hasta " << hf;
-
-                    if (currentHora >= rhi && currentHora <= rhf)
-                    {
-                        qDebug() << "Hora : ok";
-                        cma = horarios.value("cantidad_maxima_activacion").toInt();
-                        accepted = true;
-
-                        // Check Cantidad Max
-                        if(cma > 0)
-                        {
-                            int totalMarc = Bdd::countMarcacionByDate(currentFecha,rhi,rhf,&credencial);
-                            accepted = totalMarc <= cma;
-                        }
-
-                        break;
-                    }
-                }
-            }
-        }
-
-        //Check antiPassBack
-        if(antiPassBack && accepted)
-        {
-            QSqlRecord LastMarcacion = Bdd::getLastMarcacion(&credencial);
-
-            if(!LastMarcacion.isEmpty())
-            {
-                int lastSentido = LastMarcacion.value("tipo_marcacion").toInt();
-                accepted = (lastSentido != sentido);
-
-                qDebug() << "--- Result antiPassBack : " << accepted;
-            }
-        }
-
-        //Save Marcacion
-        save();
-    }
-
-    */
-
     Bdd::updatePersonaByAcceso(acceso);
     finalizeResponse();
 }
@@ -189,7 +97,6 @@ void ServiceAccess::finalizeResponse()
     {
         qDebug() << "Save Access";
         Bdd::registerAccess();
-
         Buzzer::instance()->good();
         emit openDoor();
 
@@ -198,5 +105,8 @@ void ServiceAccess::finalizeResponse()
         Buzzer::instance()->bad();
     }
 
-    emit finished();
+    QTimer *timer = new QTimer(this);
+    connect(timer, &QTimer::timeout, this, &ServiceAccess::finished);
+    connect(this, &ServiceAccess::finished, timer, &QTimer::deleteLater);
+    timer->start(1000);
 }
